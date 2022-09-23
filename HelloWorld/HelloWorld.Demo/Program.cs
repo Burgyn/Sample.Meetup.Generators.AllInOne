bb2cd9608a3859bc72f152779b02daab0a244687
Milan Martiniak <mino.martiniak@gmail.com>
2022-09-23 12:36:51 +0200

    init

---
+++ HelloWorld/HelloWorld.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.HelloWorld();
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Generator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace HelloWorld
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var myClass = @"
+using System;
+
+namespace HelloWorld.Demo
+{
+    public class MyClass
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine(""Hello World from generator ..."");
+        }
+    }
+}
+";
+            context.AddSource("MyClass", SourceText.From(myClass, Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
